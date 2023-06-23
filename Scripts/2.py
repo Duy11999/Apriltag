8f5c4d4cb5dc6048003ab8432cdb4f2805d329e9
Duy11999 <122271183+Duy11999@users.noreply.github.com>
2023-06-23 13:45:19 +0900

    Add files via upload

--- Scripts/2.py
+++ Scripts/2.py
@@ -1,52 +1,55 @@
 import cv2
 import numpy as np
 
+def nothing(x):
+    pass
 
-def find_contour_box(image_path, target_ratio):
-    # Load the image
+cv2.namedWindow("Tracking")
+cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
 
-    image = cv2.imread(image_path)
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    l_b = np.array([0, 11, 0])
-    u_b = np.array([255, 255, 255])
+while True:
+    frame = cv2.imread("../Images/red_quadrilateral_warped.jpg")
 
-    # Apply thresholding to obtain a binary image
-    mask = cv2.inRange(hsv, l_b, u_b)
+    imageLab  = cv2.cvtColor(frame, cv2.COLOR_BGR2Lab)
 
-    # Find contours in the binary image
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    l_h = cv2.getTrackbarPos("LH", "Tracking")
+    l_s = cv2.getTrackbarPos("LS", "Tracking")
+    l_v = cv2.getTrackbarPos("LV", "Tracking")
 
-    # Iterate through the contours
-    for contour in contours:
-        # Get the bounding rectangle of the contour
-        x, y, w, h = cv2.boundingRect(contour)
+    u_h = cv2.getTrackbarPos("UH", "Tracking")
+    u_s = cv2.getTrackbarPos("US", "Tracking")
+    u_v = cv2.getTrackbarPos("UV", "Tracking")
 
-        # Calculate the aspect ratio of the bounding rectangle
-        aspect_ratio = float(w) / h
+    l_b = np.array([l_h, l_s, l_v])
+    u_b = np.array([u_h, u_s, u_v])
+    mask = frame[:].copy()
+    imageRange= cv2.inRange(imageLab, l_b, u_b)
 
-        # Check if the aspect ratio matches the target ratio
-        if abs(aspect_ratio - target_ratio) < 0.1:
-            # Draw a rectangle around the contour
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-            # Display the image with the contour box
-            cv2.imshow("Contour Box", image)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-
-            # Return the coordinates of the bounding rectangle
-            return (x, y, x + w, y + h)
-
-    # If no contour with the target ratio is found, return None
-    return None
+    mask[:, :, 0] = imageRange
+    mask[:, :, 1] = imageRange
+    mask[:, :, 2] = imageRange
 
 
-# Example usage
-image_path = "../Images/Slab1.jpg"  # Replace with your image path
-target_ratio = 0.5  # Replace with your desired aspect ratio
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    faceLab = cv2.bitwise_and(frame, mask)
 
-box_coords = find_contour_box(image_path, target_ratio)
-if box_coords:
-    print("Found contour box with the specified ratio:", box_coords)
-else:
-    print("No contour box with the specified ratio was found.")
+    cv2.namedWindow(" imageRange ", cv2.WINDOW_NORMAL)
+    cv2.imshow(" imageRange ", imageRange)
+
+    cv2.namedWindow(" closing ", cv2.WINDOW_NORMAL)
+    cv2.imshow(" closing ",  closing )
+
+    cv2.namedWindow("faceLab", cv2.WINDOW_NORMAL)
+    cv2.imshow("faceLab", faceLab)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()
